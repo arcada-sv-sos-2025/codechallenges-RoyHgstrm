a2a723cb7a72ed10ebc7a76b4616ba834d197ddd
Roy <114213765+RoyHgstrm@users.noreply.github.com>
2025-03-25 23:36:29 +0200

    code challenge 1&2

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,8 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView helloTextView;
+    private Button calculateButton;
+    private ArrayList<Double> numbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        helloTextView = findViewById(R.id.hello);
+        calculateButton = findViewById(R.id.calculateButton);
+        
+        numbers = new ArrayList<>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+
+        calculateButton.setOnClickListener(v -> calculate());
+    }
+
+    private void calculate() {
+        double mean = Statistics.calcMean(numbers);
+        helloTextView.setText(String.format("Mean: %.2f", mean));
     }
 }
